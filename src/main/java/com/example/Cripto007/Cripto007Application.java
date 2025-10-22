f3e3d8d309f5bd14aa74c9efe8d099a596df247b
carloschinga <chinga_carlos@hotmail.com>
2025-10-22 14:14:39 -0500

    Proyecto Inicio configurado

---
+++ src/main/java/com/example/Cripto007/Cripto007Application.java
@@ -0,0 +1,13 @@
+package com.example.Cripto007;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cripto007Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cripto007Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Cripto007/Cripto007ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Cripto007;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cripto007ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
